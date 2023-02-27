6037df7ade11fab463109be8586b3f6c1246bd83
unknown <wtm.markowski@gmail.com>
2023-02-27 00:53:20 +0000

    Adding further comments

--- cardscores/src/main/scala/wojmark/cardscores/config/config.scala
+++ cardscores/src/main/scala/wojmark/cardscores/config/config.scala
@@ -8,10 +8,14 @@ import org.http4s.implicits._
 
 object Config {
 
+  //Helper method returning a Port provided in Start.sh
+  // Default to "8080" if not provided
   def getPort(portVariableName: String): Port = {
     Port.fromString(sys.env.getOrElse(portVariableName, "8080")).get
   }
 
+  //Helper method returning a Host address provided in Start.sh
+  // Default to "0.0.0.0" if not provided
   def getHost(hostVariableName: String): Ipv4Address = {
     (Ipv4Address
       .fromString(
@@ -20,13 +24,18 @@ object Config {
       .get
   }
 
+
+  
+
   def getUriByServiceName(serviceName: String): Uri = {
     serviceName match {
       case "CSCARDS_ENDPOINT"     => Config.getCSCardsUri()
       case "SCOREDCARDS_ENDPOINT" => Config.getScoredCardsUri()
     }
   }
-
+  
+  //Returns the uri for CSCards provided in the env file
+  //TO BE REFACTORED
   def getCSCardsUri(): Uri = {
     Uri
       .fromString(
@@ -41,6 +50,8 @@ object Config {
       )
   }
 
+   //Returns the uri for ScoredCards provided in the env file
+  //TO BE REFACTORED
   def getScoredCardsUri(): Uri = {
     Uri
       .fromString(
